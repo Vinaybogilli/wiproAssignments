7a38d802fc0b42b3214fe86a5a1f0aa14697ec00
vinay bogilli <vinaybogilli999@gmail.com>
2025-08-25 17:38:12 +0530

    addingsolid&hibernateproject

---
+++ ZomatoDeliveryAgent/src/test/java/com/wipro/vinay/config/ZomatoDeliveryAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wipro.vinay.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatoDeliveryAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
